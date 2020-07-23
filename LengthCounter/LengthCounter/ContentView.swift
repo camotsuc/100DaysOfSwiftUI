b1cd9e56c199e8f0beccf777c1c356eca28cfa7f
camotsuc <57636182+camotsuc@users.noreply.github.com>
2020-07-23 12:57:47 +0300

    Added 1-2 projects

---
+++ LengthCounter/LengthCounter/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  LengthCounter
+//
+//  Created by bixbow on 20.07.2020.
+//  Copyright © 2020 Robert Pliev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var value = ""
+    @State private var firstValueType = 0
+    @State private var secondValueType = 0
+    
+    let chooseType = ["meters","kilometers","feets","yards","miles"]
+    
+    var calculateResult: String {
+        let value = Double(self.value) ?? 0
+        let firstValue: Measurement<UnitLength>
+        var result: Measurement<UnitLength>
+        switch chooseType[firstValueType] {
+        case "meters":
+            firstValue = Measurement(value: value, unit: UnitLength.meters)
+        case "kilometers":
+            firstValue = Measurement(value: value, unit: UnitLength.kilometers)
+        case "feets":
+            firstValue = Measurement(value: value, unit: UnitLength.feet)
+        case "yards":
+            firstValue = Measurement(value: value, unit: UnitLength.yards)
+        case "miles":
+            firstValue = Measurement(value: value, unit: UnitLength.miles)
+        default:
+            firstValue = Measurement(value: 0, unit: UnitLength.miles)
+        }
+        switch chooseType[secondValueType] {
+        case "meters":
+            result = firstValue.converted(to: .meters)
+        case "kilometers":
+            result = firstValue.converted(to: .kilometers)
+        case "feets":
+            result = firstValue.converted(to: .feet)
+        case "yards":
+            result = firstValue.converted(to: .yards)
+        case "miles":
+            result = firstValue.converted(to: .miles)
+        default:
+            result = firstValue.converted(to: .meters)
+        }
+        let stripped = String(result.description.dropLast(result.description.count - 5))
+        return stripped
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter value here", text: $value)
+                        .keyboardType(.decimalPad)
+                
+                }
+                    Section(header: Text("Transfer this value")) {
+                        Picker("Value", selection: $firstValueType) {
+                            ForEach(0 ..< chooseType.count) {
+                                Text("\(self.chooseType[$0])")
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    Section(header: Text("To this value")) {
+                        Picker("Value", selection: $secondValueType) {
+                            ForEach(0 ..< chooseType.count) {
+                                Text("\(self.chooseType[$0])")
+                                    
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                    Section(header: Text("Result is")) {
+                        Text("\(calculateResult)")
+                    }
+                }
+                .navigationBarTitle("LengthCounter")
+            }
+        }
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,72 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by bixbow on 15.07.2020.
+//  Copyright © 2020 Robert Pliev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPecentage = 2
+    @State private var tipPecentages = [10,15,20,25,0]
+    
+    var eachAmount: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        let tipSelection = Double(tipPecentages[tipPecentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let tip = Double(tipPecentages[tipPecentage]) / 100
+        let amount = Double(checkAmount) ?? 0
+        let result = amount * tip
+        
+        return result + amount
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount: ", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+
+                    TextField("Peoples", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Tip %")) {
+                    Picker("TipPercentage", selection: $tipPecentage) {
+                        ForEach(0 ..< tipPecentages.count) {
+                            Text("\(self.tipPecentages[$0]) %")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(eachAmount, specifier: "%.2f")")
+                }
+                Section(header: Text("Total amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
