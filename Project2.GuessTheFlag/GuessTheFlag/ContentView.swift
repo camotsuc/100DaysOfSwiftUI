c6c3389cab04339a4b6a03f1eae2252d0f0e1718
camotsuc <57636182+camotsuc@users.noreply.github.com>
2020-07-25 20:37:27 +0300

    Updated. Day 24

--- Project2.GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2.GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,6 +8,17 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var image: String
+    var body: some View {
+        Image(image)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black))
+            .shadow(color: .black, radius: 2)
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -39,11 +50,7 @@ struct ContentView: View {
                         self.flagTapped(number)
                         self.tappedCountry = self.countries[number]
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(image: self.countries[number])
                     }
                 }
                 Text("Your score is \(userScore)")
