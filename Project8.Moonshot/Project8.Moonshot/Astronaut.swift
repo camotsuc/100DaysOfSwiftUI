83504e492304f07ee974dc7f7d8aee87191a99ac
camotsuc <57636182+camotsuc@users.noreply.github.com>
2020-08-22 14:57:33 +0300

    Added project 8

---
+++ Project8.Moonshot/Project8.Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Project8.Moonshot
+//
+//  Created by camotsuc on 22.08.2020.
+//  Copyright © 2020 robertpliev@gmail.com. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
---
+++ Project8.Moonshot/Project8.Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,30 @@
+//
+//  Bundle-Decodable.swift
+//  Project8.Moonshot
+//
+//  Created by camotsuc on 22.08.2020.
+//  Copyright © 2020 robertpliev@gmail.com. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Faled to locate the \(file)")
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Faled to load the \(file)")
+        }
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
