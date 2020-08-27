f492c8b9b16b34d2b8babd9c5e9d72a4034baa94
camotsuc <57636182+camotsuc@users.noreply.github.com>
2020-08-27 23:12:12 +0300

    Renamed folder

---
+++ Project9.Drawing/Project9.Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project9.Drawing
+//
+//  Created by bixbow on 27.08.2020.
+//  Copyright © 2020 robertpliev@gmail.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
